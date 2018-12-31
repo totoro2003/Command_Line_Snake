f15adca907fb23ae81e415268fc57bef8baa3e3d
totoro2003 <39471893+totoro2003@users.noreply.github.com>
2018-12-31 15:42:01 -0500

    removed things

--- snake_part.h
+++ snake_part.h
@@ -6,14 +6,13 @@
 class snake_part
 {
 public:
-    snake_part(HANDLE console_handle);
-    void set_position(COORD new_position);
+    snake_part(HANDLE * console_handle);
     void set_position(COORD * new_position);
     COORD * get_position();
     int draw();
 private:
     COORD * snake_part_position;
-    HANDLE console_handle;
+    HANDLE * console_handle;
 };
 
 #endif // SNAKE_PART_H
